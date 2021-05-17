b7309cd1777514bf2bcf36c14309968d91291f64
Vent-Tardar <79703422+Vent-Tardar@users.noreply.github.com>
2021-05-17 12:54:58 +0300

    Update Main.java

--- src/com/practic/vt/two/Main.java
+++ src/com/practic/vt/two/Main.java
@@ -2,6 +2,6 @@ package com.practic.vt.two;
 
 public class Main {
     public static void main(String[] args){
-        LaunchPage launchPage = new LaunchPage();
+        new LaunchPage();
     }
 }
